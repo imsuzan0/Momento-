6a6e23bf55044816dab4a856d837cfeb518cb044
Sujan Timalsina <sujantimalsina333@gmail.com>
2025-03-14 21:43:14 +0545

    project setup!!!

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,14 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { Link } from 'expo-router'
+
+const index = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center',gap: 100 }}>
+      <Link href={"/notification"}>Visit Notification Screen</Link>
+      <Link href={"/profile"}>Visit Profile Screen</Link>
+    </View>
+  )
+}
+
+export default index
