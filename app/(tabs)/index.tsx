9120acca433344ecfc52a00fc0646446a663007f
Sujan Timalsina <sujantimalsina333@gmail.com>
2025-03-19 22:42:04 +0545

    Implement feed posts display with loading and no posts states, and add story section

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,64 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { Link } from 'expo-router'
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  ScrollView,
+  FlatList,
+} from "react-native";
+import React from "react";
+import { styles } from "@/styles/feed.style";
+import { useAuth } from "@clerk/clerk-expo";
+import { Ionicons } from "@expo/vector-icons";
+import { COLORS } from "@/constants/theme";
+import { STORIES } from "@/constants/mock-data";
+import Story from "@/components/Story";
+import { useQuery } from "convex/react";
+import { api } from "@/convex/_generated/api";
+import Loader from "@/components/Loader";
+import NoPostsFound from "@/components/NoPostsFound";
+import Post from "@/components/Post";
+// import { getFeedPosts } from "@/convex/posts";
 
 const index = () => {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center',gap: 100 }}>
-      <Text>Home</Text>
-    </View>
-  )
-}
+  const { signOut } = useAuth();
 
-export default index
+  const posts = useQuery(api.posts.getFeedPosts);
+
+  if (posts === undefined) return <Loader />;
+
+  if (posts.length === 0) return <NoPostsFound />;
+
+  return (
+    <View style={styles.container}>
+      {/* Header Section */}
+      <View style={styles.header}>
+        <Text style={styles.headerTitle}>Momento</Text>
+        <TouchableOpacity onPress={() => signOut()}>
+          <Ionicons name="log-out-outline" size={24} color={COLORS.white} />
+        </TouchableOpacity>
+      </View>
+
+      <ScrollView showsVerticalScrollIndicator={false}>
+        {/*Story Section */}
+
+        <FlatList
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          style={styles.storiesContainer}
+          data={STORIES} // Pass your STORIES array here
+          keyExtractor={(item) => item.id} // Extract a unique key for each item
+          renderItem={({ item }) => (
+            <Story key={item.id} story={item} /> // Render your Story component
+          )}
+        />
+
+        {posts.map((post) => (
+          <Post key={post._id} post={post} />
+        ))}
+      </ScrollView>
+    </View>
+  );
+};
+
+export default index;
+
